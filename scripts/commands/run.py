6d820e8659b883a26c32c74b9639ef93f283d44b
Musca Mihail <muscamihaill@gmail.com>
2025-05-02 16:13:13 +0300

    fix build, package & run commands

--- scripts/commands/run.py
+++ scripts/commands/run.py
@@ -7,7 +7,7 @@ from utils import project
 def run():
     print("Running...")
 
-    if os.path.exists(f"build/{project.get_triplet()}/{config.PACKAGE_EXECUTABLE.get_path()}"):
-        subprocess.run([f"build/{project.get_triplet()}/{config.PACKAGE_EXECUTABLE.get_path()}"])
+    if os.path.exists(f"build/{project.get_triplet()}/{project.CONFIGURATION}/{config.PACKAGE_EXECUTABLE.get_path()}"):
+        subprocess.run([f"build/{project.get_triplet()}/{project.CONFIGURATION}/{config.PACKAGE_EXECUTABLE.get_path()}"])
     else:
         print("No build found.")
