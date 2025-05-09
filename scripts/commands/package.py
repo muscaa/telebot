83a88f147f15808bbeb2b31571a89b6d3c4f27da
Musca Mihail <muscamihaill@gmail.com>
2025-05-09 22:24:39 +0300

    improve package command

--- scripts/commands/package.py
+++ scripts/commands/package.py
@@ -7,31 +7,36 @@ from utils import project
 def package():
     print(f"Packaging {project.get_package_zip_name()}...")
 
+    added_files = {}
     build_dir = f"build/{project.get_triplet()}/{project.CONFIGURATION}"
     with zipfile.ZipFile(f"build/{project.get_package_zip_name()}", "w", zipfile.ZIP_DEFLATED) as zip:
-        added_files = set()
-
         if config.PACKAGE_EXECUTABLE.is_valid():
             src = os.path.join(build_dir, config.PACKAGE_EXECUTABLE.get_path())
             dst = config.PACKAGE_EXECUTABLE.get_path_to()
             zip.write(src, dst)
-            added_files.add(os.path.normpath(src))
+            added_files[src] = dst
 
         for lib in config.PACKAGE_LIBRARIES:
             if lib.is_valid():
                 src = os.path.join(build_dir, lib.get_path())
                 dst = lib.get_path_to()
                 zip.write(src, dst)
-                added_files.add(os.path.normpath(src))
+                added_files[src] = dst
 
         for file in config.PACKAGE_OTHER:
             if file.is_valid():
-                zip.write(file.get_path(), file.get_path_to())
+                src = file.get_path()
+                dst = file.get_path_to()
+                zip.write(src, dst)
+                added_files[src] = dst
 
         for root, _, files in os.walk(build_dir):
             for file in files:
-                full_path = os.path.normpath(os.path.join(root, file))
-                if full_path not in added_files:
-                    rel_path = os.path.relpath(full_path, build_dir)
-                    zip.write(full_path, rel_path)
-    
+                src = os.path.join(root, file)
+                if src not in added_files:
+                    dst = os.path.relpath(src, build_dir)
+                    zip.write(src, dst)
+                    added_files[src] = dst
+
+    for src, dst in added_files.items():
+        print(f"{src} -> {dst}")
