a9f1e610917296897c3245f433cdc060304c0dd4
Musca Mihail <muscamihaill@gmail.com>
2025-05-15 18:49:09 +0300

    changed plugins window size

--- api/src/telebot/render/imgui.cpp
+++ api/src/telebot/render/imgui.cpp
@@ -9,7 +9,7 @@
 namespace telebot::render::imgui {
 
 static void window_plugins() {
-    ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(600, 400), ImGuiCond_FirstUseEver);
     if (!ImGui::Begin("Plugins")) {
         ImGui::End();
         return;
