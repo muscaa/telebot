84b1ac164475f78897ce95a04297e2121312dc00
Musca Mihail <muscamihaill@gmail.com>
2025-05-22 15:11:14 +0300

    added tcp-server-client lib

---
+++ api/lib/tcp_server_client/src/TcpConnection.cpp
@@ -0,0 +1,90 @@
+#include "TcpConnection.hpp"
+
+#include <iostream>
+
+namespace {
+constexpr auto f_readBufferSize{1024};
+}  // namespace
+
+void TcpConnection::Observer::onReceived([[maybe_unused]] int connectionId,
+                                         [[maybe_unused]] const char *data,
+                                         [[maybe_unused]] const size_t size) {}
+
+void TcpConnection::Observer::onConnectionClosed(
+    [[maybe_unused]] int connectionId) {}
+
+TcpConnection::TcpConnection(boost::asio::ip::tcp::socket &&socket, Observer &observer, int id)
+    : m_socket{std::move(socket)},
+      m_readBuffer{},
+      m_writeBuffer{},
+      m_writeBufferMutex{},
+      m_observer{observer},
+      m_isReading{false},
+      m_isWritting{false},
+      m_id{id} {}
+
+std::shared_ptr<TcpConnection> TcpConnection::create(boost::asio::ip::tcp::socket &&socket, Observer &observer, int id) {
+    return std::shared_ptr<TcpConnection>(new TcpConnection{std::move(socket), observer, id});
+}
+
+void TcpConnection::startReading() {
+    if (!m_isReading) {
+        doRead();
+    }
+}
+
+void TcpConnection::send(const char *data, size_t size) {
+    std::lock_guard<std::mutex> guard{m_writeBufferMutex};
+    std::ostream bufferStream{&m_writeBuffer};
+    bufferStream.write(data, size);
+    if (!m_isWritting) {
+        doWrite();
+    }
+}
+
+void TcpConnection::close() {
+    try {
+        m_socket.cancel();
+        m_socket.close();
+    } catch (const std::exception &e) {
+        std::cerr << "TcpConnection::close() exception: " + static_cast<std::string>(e.what()) + ".\n";
+        return;
+    }
+    m_observer.onConnectionClosed(m_id);
+}
+
+void TcpConnection::doRead() {
+    m_isReading = true;
+    auto buffers{m_readBuffer.prepare(f_readBufferSize)};
+    auto self{shared_from_this()};
+    m_socket.async_read_some(buffers, [this, self](const auto &error, auto bytesTransferred) {
+        if (error) {
+            std::cerr << "TcpConnection::doRead() error: " + error.message() + ".\n";
+            return close();
+        }
+        m_readBuffer.commit(bytesTransferred);
+        m_observer.onReceived(m_id, static_cast<const char *>(m_readBuffer.data().data()), bytesTransferred);
+        m_readBuffer.consume(bytesTransferred);
+        doRead();
+    });
+}
+
+void TcpConnection::doWrite() {
+    m_isWritting = true;
+    auto self{shared_from_this()};
+    m_socket.async_write_some(
+        m_writeBuffer.data(),
+        [this, self](const auto &error, auto bytesTransferred) {
+            if (error) {
+                std::cerr << "TcpConnection::doWrite() error: " + error.message() + ".\n";
+                return close();
+            }
+            std::lock_guard<std::mutex> guard{m_writeBufferMutex};
+            m_writeBuffer.consume(bytesTransferred);
+            if (m_writeBuffer.size() == 0) {
+                m_isWritting = false;
+                return;
+            }
+            doWrite();
+        });
+}
---
+++ api/lib/tcp_server_client/src/TcpServer.cpp
@@ -0,0 +1,92 @@
+#include "TcpServer.hpp"
+
+#include <iostream>
+#include <string>
+
+void TcpServer::Observer::onConnectionAccepted(
+    [[maybe_unused]] int connectionId) {}
+
+void TcpServer::Observer::onReceived([[maybe_unused]] int connectionId,
+                                     [[maybe_unused]] const char *data,
+                                     [[maybe_unused]] const size_t size) {}
+
+void TcpServer::Observer::onConnectionClosed([[maybe_unused]] int connectionId) {}
+
+TcpServer::TcpServer(boost::asio::io_context &ioContext, Observer &observer)
+    : m_acceptor{ioContext},
+      m_connections{},
+      m_observer{observer},
+      m_connectionCount{0},
+      m_isAccepting{false},
+      m_isClosing{false} {}
+
+bool TcpServer::listen(const boost::asio::ip::tcp &protocol, uint16_t port) {
+    try {
+        m_acceptor.open(protocol);
+        m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+        m_acceptor.bind({protocol, port});
+        m_acceptor.listen(boost::asio::socket_base::max_listen_connections);
+    } catch (const std::exception &e) {
+        std::cerr << "TcpServer::listen() exception: " + static_cast<std::string>(e.what()) + ".\n";
+        return false;
+    }
+    return true;
+}
+
+void TcpServer::startAcceptingConnections() {
+    if (!m_isAccepting) {
+        doAccept();
+    }
+}
+
+void TcpServer::send(int connectionId, const char *data, size_t size) {
+    if (m_connections.count(connectionId) == 0) {
+        std::cerr << "TcpServer::send() error: connection not found.\n";
+        return;
+    }
+    m_connections.at(connectionId)->send(data, size);
+}
+
+void TcpServer::close() {
+    m_isClosing = true;
+    m_acceptor.cancel();
+    for (const auto &connection : m_connections) {
+        connection.second->close();
+    }
+    m_connections.clear();
+    m_isClosing = false;
+    std::cout << "TCPServer disconnected.\n";
+}
+
+void TcpServer::doAccept() {
+    m_isAccepting = true;
+    m_acceptor.async_accept([this](const auto &error, auto socket) {
+        if (error) {
+            std::cerr << "TcpServer::doAccept() error: " + error.message() + ".\n";
+            m_isAccepting = false;
+            return;
+        } else {
+            auto connection{TcpConnection::create(std::move(socket), *this, m_connectionCount)};
+            connection->startReading();
+            m_connections.insert({m_connectionCount, std::move(connection)});
+            std::cout << "TCPServer accepted connection.\n";
+            m_observer.onConnectionAccepted(m_connectionCount);
+            m_connectionCount++;
+        }
+        doAccept();
+    });
+}
+
+void TcpServer::onReceived(int connectionId, const char *data, size_t size) {
+    m_observer.onReceived(connectionId, data, size);
+}
+
+void TcpServer::onConnectionClosed(int connectionId) {
+    if (m_isClosing) {
+        return;
+    }
+    if (m_connections.erase(connectionId) > 0) {
+        std::cout << "TCPServer removed connection.\n";
+        m_observer.onConnectionClosed(connectionId);
+    }
+}
