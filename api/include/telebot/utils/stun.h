264a114d520ba428e6dcd9d9bcff3acf9359137c
Musca Mihail <muscamihaill@gmail.com>
2025-05-30 19:53:06 +0300

    added linking capability

--- api/include/telebot/utils/stun.h
+++ api/include/telebot/utils/stun.h
@@ -11,6 +11,10 @@ namespace tcp = telebot::utils::tcp;
 
 const uint8_t PACKET_S2C_LIST = 0x00;
 const uint8_t PACKET_C2S_LOGIN = 0x01;
+const uint8_t PACKET_C2S_LINK_REQUEST = 0x02;
+const uint8_t PACKET_S2C_LINK_REQUEST = 0x03;
+const uint8_t PACKET_C2S_LINK_RESPONSE = 0x04;
+const uint8_t PACKET_S2C_LINK_RESPONSE = 0x05;
 
 struct ClientListener;
 class Client;
@@ -26,7 +30,21 @@ struct ClientListener : public tcp::ClientListener {
 
     virtual void sendC2SLogin(Client* client, const std::string& name);
 
+    virtual void sendC2SLinkRequest(Client* client, const std::string& name);
+
+    virtual void readS2CLinkRequest(Client* client, const uint8_t* data, size_t& offset);
+
+    virtual void sendC2SLinkResponse(Client* client, const std::string& name, bool accept);
+
+    virtual void readS2CLinkResponse(Client* client, const uint8_t* data, size_t& offset);
+
     virtual void onList(Client* client, const std::vector<std::string>& list) = 0;
+
+    virtual void onLinkRequest(Client* client, const std::string& from) = 0;
+
+    virtual void onLinkAccepted(Client* client, const std::string& name, const std::string& ip, uint16_t port) = 0;
+
+    virtual void onLinkDeclined(Client* client, const std::string& name, const std::string& message) = 0;
 };
 
 class Client : public tcp::Client {
@@ -54,11 +72,21 @@ struct ServerListener : public tcp::ServerListener {
     virtual void sendS2CList(Server* server);
 
     virtual void readC2SLogin(Server* server, int id, const uint8_t* data, size_t& offset);
+
+    virtual void readC2SLinkRequest(Server* server, int id, const uint8_t* data, size_t& offset);
+
+    virtual void sendS2CLinkRequest(Server* server, const std::string& from, const std::string& to);
+
+    virtual void readC2SLinkResponse(Server* server, int id, const uint8_t* data, size_t& offset);
+
+    virtual void sendS2CLinkResponse(Server* server, const std::string& from, const std::string& to, bool accept, const std::string& declineMessage = "Link request declined");
 };
 
 class Server : public tcp::Server {
    public:
-    std::unordered_map<std::string, int> list;
+    std::unordered_map<int, std::string> id2Name;
+    std::unordered_map<std::string, int> name2Id;
+    std::unordered_map<int, int> linkRequests;
 
     Server(const std::shared_ptr<ServerListener>& listener)
         : tcp::Server(listener) {}
--- api/src/telebot/utils/stun.cpp
+++ api/src/telebot/utils/stun.cpp
@@ -27,6 +27,12 @@ void ClientListener::onReceived(tcp::Client* client, const uint8_t* data, size_t
         case PACKET_S2C_LIST:
             readS2CList(stunClient, data, offset);
             break;
+        case PACKET_S2C_LINK_REQUEST:
+            readS2CLinkRequest(stunClient, data, offset);
+            break;
+        case PACKET_S2C_LINK_RESPONSE:
+            readS2CLinkResponse(stunClient, data, offset);
+            break;
         default:
             log::error("Unknown packet ID: {}", packetId);
             break;
@@ -58,6 +64,49 @@ void ClientListener::sendC2SLogin(Client* client, const std::string& name) {
     client->send(data, offset);
 }
 
+void ClientListener::sendC2SLinkRequest(Client* client, const std::string& name) {
+    uint8_t data[1024];
+    size_t offset = 0;
+
+    bin::Byte(data, offset, PACKET_C2S_LINK_REQUEST);
+    bin::LenString(data, offset, name);
+
+    client->send(data, offset);
+}
+
+void ClientListener::readS2CLinkRequest(Client* client, const uint8_t* data, size_t& offset) {
+    std::string from = bin::LenString(data, offset);
+
+    onLinkRequest(client, from);
+}
+
+void ClientListener::sendC2SLinkResponse(Client* client, const std::string& name, bool accept) {
+    uint8_t data[1024];
+    size_t offset = 0;
+
+    bin::Byte(data, offset, PACKET_C2S_LINK_RESPONSE);
+    bin::LenString(data, offset, name);
+    bin::Bool(data, offset, accept);
+
+    client->send(data, offset);
+}
+
+void ClientListener::readS2CLinkResponse(Client* client, const uint8_t* data, size_t& offset) {
+    std::string name = bin::LenString(data, offset);
+    bool accept = bin::Bool(data, offset);
+
+    if (accept) {
+        std::string ip = bin::LenString(data, offset);
+        uint16_t port = bin::Short(data, offset);
+
+        onLinkAccepted(client, name, ip, port);
+    } else {
+        std::string declineMessage = bin::LenString(data, offset);
+
+        onLinkDeclined(client, name, declineMessage);
+    }
+}
+
 // server
 
 void ServerListener::onConnectionAccepted(tcp::Server* server, int id) {
@@ -73,6 +122,12 @@ void ServerListener::onReceived(tcp::Server* server, int id, const uint8_t* data
         case PACKET_C2S_LOGIN:
             readC2SLogin(stunServer, id, data, offset);
             break;
+        case PACKET_C2S_LINK_REQUEST:
+            readC2SLinkRequest(stunServer, id, data, offset);
+            break;
+        case PACKET_C2S_LINK_RESPONSE:
+            readC2SLinkResponse(stunServer, id, data, offset);
+            break;
         default:
             log::error("Unknown packet ID: {}", packetId);
             break;
@@ -80,14 +135,17 @@ void ServerListener::onReceived(tcp::Server* server, int id, const uint8_t* data
 }
 
 void ServerListener::onConnectionClosed(tcp::Server* server, int id) {
+    log::info("Client with ID {} left", id);
+
     Server* stunServer = server->as<Server>();
 
-    for (auto it = stunServer->list.begin(); it != stunServer->list.end(); it++) {
-        if (it->second == id) {
-            stunServer->list.erase(it);
-            sendS2CList(stunServer);
-            break;
-        }
+    if (stunServer->id2Name.contains(id)) {
+        std::string name = stunServer->id2Name[id];
+
+        stunServer->id2Name.erase(id);
+        stunServer->name2Id.erase(name);
+
+        sendS2CList(stunServer);
     }
 }
 
@@ -97,8 +155,8 @@ void ServerListener::sendS2CList(Server* server) {
 
     bin::Byte(data, offset, PACKET_S2C_LIST);
 
-    bin::Int(data, offset, static_cast<int32_t>(server->list.size()));
-    for (const auto& [name, id] : server->list) {
+    bin::Int(data, offset, static_cast<int32_t>(server->id2Name.size()));
+    for (const auto& [id, name] : server->id2Name) {
         bin::LenString(data, offset, name);
     }
 
@@ -108,16 +166,127 @@ void ServerListener::sendS2CList(Server* server) {
 void ServerListener::readC2SLogin(Server* server, int id, const uint8_t* data, size_t& offset) {
     std::string name = bin::LenString(data, offset);
 
-    if (server->list.find(name) != server->list.end()) {
+    if (server->name2Id.contains(name)) {
         log::error("Client with name '{}' already exists", name);
         server->getConnections()[id]->close();
         return;
     }
 
-    server->list[name] = id;
+    server->id2Name[id] = name;
+    server->name2Id[name] = id;
 
     log::info("Client with ID {} connected as '{}'", id, name);
     sendS2CList(server);
+}
+
+void ServerListener::readC2SLinkRequest(Server* server, int id, const uint8_t* data, size_t& offset) {
+    if (!server->id2Name.contains(id)) {
+        log::error("Client with ID {} not logged in", id);
+        server->getConnections()[id]->close();
+        return;
+    }
+
+    std::string from = server->id2Name[id];
+    std::string to = bin::LenString(data, offset);
+
+    if (!server->name2Id.contains(to)) {
+        log::error("Client with name '{}' not found", to);
+        server->getConnections()[id]->close();
+        return;
+    }
+
+    sendS2CLinkRequest(server, from, to);
+}
+
+void ServerListener::sendS2CLinkRequest(Server* server, const std::string& from, const std::string& to) {
+    int idFrom = server->name2Id[from];
+    int idTo = server->name2Id[to];
+
+    if (server->linkRequests.contains(idFrom) || server->linkRequests.contains(idTo)) {
+        log::error("Cannot send link request from '{}' to '{}'", from, to);
+        return;
+    }
+
+    server->linkRequests[idFrom] = idTo;
+
+    uint8_t data[1024];
+    size_t offset = 0;
+
+    bin::Byte(data, offset, PACKET_S2C_LINK_REQUEST);
+    bin::LenString(data, offset, from);
+
+    server->send(idTo, data, offset);
+}
+
+void ServerListener::readC2SLinkResponse(Server* server, int id, const uint8_t* data, size_t& offset) {
+    std::string from = bin::LenString(data, offset);
+    int idFrom = server->name2Id[from];
+    std::string to = server->id2Name[id];
+    int idTo = id;
+
+    if (!server->linkRequests.contains(idFrom) || server->linkRequests[idFrom] != idTo) {
+        log::error("Link request from '{}' to '{}' not found", from, to);
+        server->getConnections()[id]->close();
+        return;
+    }
+
+    server->linkRequests.erase(idFrom);
+
+    bool accept = bin::Bool(data, offset);
+    if (accept) {
+        sendS2CLinkResponse(server, from, to, true);
+    } else {
+        sendS2CLinkResponse(server, from, to, false);
+    }
+}
+
+void ServerListener::sendS2CLinkResponse(Server* server, const std::string& from, const std::string& to, bool accept, const std::string& declineMessage) {
+    int idFrom = server->name2Id[from];
+    int idTo = server->name2Id[to];
+
+    uint8_t data[1024];
+    size_t offset = 0;
+
+    bin::Byte(data, offset, PACKET_S2C_LINK_RESPONSE);
+
+    if (!accept) {
+        bin::LenString(data, offset, to);
+        bin::Bool(data, offset, false);
+        bin::LenString(data, offset, declineMessage);
+
+        server->send(idFrom, data, offset);
+        return;
+    }
+
+    {
+        bin::LenString(data, offset, to);
+        bin::Bool(data, offset, true);
+
+        boost::asio::ip::tcp::endpoint endpointTo = server->getConnections()[idTo]->getSocket().remote_endpoint();
+        std::string ipTo = endpointTo.address().to_string();
+        uint16_t portTo = endpointTo.port();
+
+        bin::LenString(data, offset, ipTo);
+        bin::Short(data, offset, portTo);
+
+        server->send(idFrom, data, offset);
+    }
+
+    offset = 1;
+
+    {
+        bin::LenString(data, offset, from);
+        bin::Bool(data, offset, true);
+
+        boost::asio::ip::tcp::endpoint endpointFrom = server->getConnections()[idFrom]->getSocket().remote_endpoint();
+        std::string ipFrom = endpointFrom.address().to_string();
+        uint16_t portFrom = endpointFrom.port();
+
+        bin::LenString(data, offset, ipFrom);
+        bin::Short(data, offset, portFrom);
+
+        server->send(idTo, data, offset);
+    }
 }
 
 } // namespace telebot::utils::stun
